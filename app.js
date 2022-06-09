1bc07436c4f6a28173025ff27b1ce31c35648d3a
Justin <ju3tin@hotmail.co.uk>
2022-06-09 18:59:48 +0100

    make it better

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
 const path = require('path');
 const express = require('express');
 const cors = require('cors');
-app.use(cors());
 const app = express();
 const publicPath = path.join(__dirname, '..', 'public');
 const port = process.env.PORT || 3000;
+app.use(cors());
 app.use(express.static(publicPath));
 app.get('*', (req, res) => {
    res.sendFile(path.join(publicPath, 'index.html'));
