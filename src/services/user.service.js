50e98ad81f6f7bb5294e6a773cebcaa886ede40e
Justin <ju3tin@hotmail.co.uk>
2022-06-09 19:39:30 +0100

    make it better

--- src/services/user.service.js
+++ src/services/user.service.js
@@ -12,7 +12,7 @@ class UserService {
       "Content-Type": "application/json",
       "Access-Control-Allow-Origin": 'http://localhost:8081',
       "Access-Control-Allow-Methods": "GET, POST, PATCH, PUT, DELETE, OPTIONS",
-      "Access-Control-Allow-Headers": "Origin, Content-Type, X-Auth-Token, Authorization, Accept,charset,boundary,Content-Length"
+      "Access-Control-Allow-Headers": "Host, Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token,X-XSRF-TOKEN, Origin, Access-Control-Request-Origin, Access-Control-Request-Method, Access-Control-Request-Headers, Access-Control-Allow-Origin, access-control-allow-origin, Access-Control-Allow-Credentials, access-control-allow-credentials, Access-Control-Allow-Headers, access-control-allow-headers, Access-Control-Allow-Methods, access-control-allow-methods"
   }
 }
   );
